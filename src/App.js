2c51a12b44ceccf2c132ae9a43687c6c7d2a4a37
unknown <mabhi5670@gmail.com>
2021-11-25 17:33:29 +0530

    modal for login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-function App() {
+import Navbar from './components/Navbar/Navbar';
+import Button from './Asset/Button/Button';
+import { useState } from 'react';
+import axios from 'axios';
+const App = () => {
+  const [setUser] = useState({});
+
+  const getUser = async () => {
+    const res = await axios.get(
+      `https://api.unsplash.com/photos/random/?&client_id=${process.env.Client_ID}`
+    );
+    console.log('data>>>', res);
+    setUser(res);
+    console.log('data>>>', res);
+  };
+
+  console.log('<<<get', getUser);
   return (
     <div className='App'>
-      <PinterestIcon />
+      <Navbar />
+      <Button />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,202 @@
+import React from 'react';
+import styles from './Login.module.css';
+import styles1 from '../Navbar/Navbar.module.css';
+import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import GoogleIcon from '@mui/icons-material/Google';
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #fff',
+  boxShadow: 24,
+  p: 2,
+  minHeight: '600px',
+  minWidth: '430px',
+  borderRadius: '30px',
+  modal: {
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+};
+
+const useStyles = makeStyles((theme) => ({
+  myClassName: {
+    '&:hover': {
+      backgroundColor: '#d3cfd0',
+    },
+  },
+}));
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(32).required(),
+});
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmitHandler = (data) => {
+    console.log({ data });
+    reset();
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const classes = useStyles();
+
+  return (
+    <div>
+      <span className={styles.logintxt} onClick={handleOpen}>
+        Login
+      </span>
+      <Modal
+        open={open}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <Typography
+            id='modal-modal-title'
+            variant='h6'
+            component='h2'
+            onClick={handleClose}
+            display='flex'
+            justifyContent='flex-end'
+          >
+            <IconButton color='inherit' className={classes.myClassName}>
+              <CloseIcon sx={{ stroke: 'black', strokeWidth: '1' }} />
+            </IconButton>
+          </Typography>
+          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+            <form
+              onSubmit={handleSubmit(onSubmitHandler)}
+              className={styles.form}
+            >
+              <div>
+                <PinterestIcon
+                  className={styles.logo}
+                  sx={{
+                    width: 40,
+                    height: 40,
+                    position: 'relative',
+                    bottom: '20px',
+                  }}
+                />
+              </div>
+              <h2 className={styles.headertxt}>Welcome to Pinterest</h2>
+              <br />
+
+              <input
+                {...register('email')}
+                placeholder='Email'
+                type='email'
+                required
+                className={styles.input}
+              />
+
+              <p>{errors.email?.message}</p>
+              <br />
+
+              <input
+                {...register('password')}
+                placeholder='password'
+                type='password'
+                required
+                className={styles.inputPass}
+              />
+              <a href='/' className={styles.forgetxt}>
+                Forgotten your password?
+              </a>
+              <p>{errors.password?.message}</p>
+              <br />
+
+              <div className={styles.logintxt}>Log In</div>
+            </form>
+            <div className={styles.orTxt}>
+              <span className={styles.Txt}>Or</span>
+
+              <span className={styles.facebook}>
+                <FacebookRoundedIcon
+                  sx={{
+                    color: '#fff',
+                    marginRight: '8px',
+                  }}
+                />
+                Connect With Facebook
+              </span>
+
+              <span className={styles.google}>
+                <GoogleIcon
+                  sx={{
+                    fontSize: '20px',
+                    marginRight: '8px',
+                  }}
+                />
+                Connect With Google
+              </span>
+              <span className={styles.acknowledge}>
+                By continuing, you agree to Pinterest's
+              </span>
+              <span className={styles.termsText}>
+                <a
+                  href='/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={styles.terms}
+                >
+                  Terms of Service
+                </a>
+                <span className={styles.acknowledge}>
+                  and acknowledge that you've read
+                </span>
+              </span>
+              <span>
+                <span className={styles.privacy}>our</span>
+                <a
+                  href='/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={styles.terms}
+                >
+                  Privacy Policy
+                </a>
+                <hr />
+              </span>
+
+              <span className={styles.footerTxt}>
+                <span>Not on Pinterest yet? Sign-up</span>
+                <span className={styles.getStartedTxt}>
+                  Are you a business? Get started here!
+                </span>
+              </span>
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default Login;
